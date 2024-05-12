c17ab4d5156380176c8ac032e558507866a0a280
Mikko Salmi <mikko.salmi@avanio.com>
2024-05-12 22:11:36 +0300

    Page Up/Down Center line

--- lua/laiskimus/remap.lua
+++ lua/laiskimus/remap.lua
@@ -5,5 +5,7 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 -- vim.keymap.set("x", "<leader>p", [["_dP]])
 -- vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 -- vim.keymap.set("n", "<leader>Y", [["+Y]])
-
+-- Page Up/Down Center line
+vim.keymap.set("n","<C-d>", "<C-d>zz")
+vim.keymap.set("n","<C-u>", "<C-u>zz")
  
